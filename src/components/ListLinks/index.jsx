d0d61f51a8516ab7fa494e18212e84e0005742ff
Kelvys Moura <kelvysmoura2@gmail.com>
2024-05-07 20:24:39 -0300

    Criando e usando contexto

--- src/components/ListLinks/index.jsx
+++ src/components/ListLinks/index.jsx
@@ -1,13 +1,16 @@
 
 import LinkItem from "../LinkItem";
 import storage from "../../storage";
+import { ThemeContext } from "../../context/ThemeContext";
+import { useContext } from "react";
 
 const links = storage.getLinks();
 
-
 export default function ListLinks() {
+    let theme = useContext(ThemeContext);
+
     return (
-        <ul className="list-links">
+        <ul className={`theme-${theme} list-links`}>
             { links.map((item, index) => (<LinkItem key={index} {...item} />) ) }
         </ul>
     )
--- src/pages/LinksPage/index.jsx
+++ src/pages/LinksPage/index.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import ListLinks from "../../components/ListLinks";
 import './LinksPage.css';
+import { ThemeContext } from "../../context/ThemeContext";
+import { useState } from "react";
 
 export default function LinksPage() {
+    const [theme, setTheme] = useState('light');
     return (
-        <>
+        <ThemeContext.Provider value={theme}>
             <Link to="/login">Login</Link>
             <ListLinks />
-        </>
+            <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>Toggle</button>
+        </ThemeContext.Provider>
     )
 }
